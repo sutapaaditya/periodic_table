63b7ca2eeaa136ab2cd9dabdd082b969018aae79
barungh <barungh@hotmail.com>
2022-08-30 20:30:59 +0530

    Final version 😎

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const data = [ 
+  {
+  "Hydrogen" : [ "../images/nebula.jpg", 
+    `Hydrogen is the number one element of the periodic table and earns
+this status for a range of reasons: along with helium and lithium, it
+was one of the first three elements produced during the BigBang; it
+is the most abundant element in the universe, accounting for 88
+percent of all atoms; and it is the lightest of all the elements, with
+only one proton (which is why it is number 1 in the periodic table)
+and one electron`
+  ]
+},
+  {
+    "Helium" : [ "../images/jupiter.png",
+    `Helium is named for the Greek God of the sun, Helios, after it was
+first detected as unknown yellow spectral lines in sunlight. In India
+in 1868, French astronomer Jules Janssen passed sunlight through
+a prism during a solar eclipse (to split the light into its component
+colors). He found a sudden jump in the brightness of yellow light,
+which he initially believed to be caused by sodium`
+      ]
+  }
+] 
--- js/index.js
+++ js/index.js
@@ -1,25 +1,33 @@
-document
-  .getElementById("h")
-  .addEventListener('click', () => {
-  document.querySelector('.element-details').style.display = "flex";
-})
+let showDetails = (m) => {
+  let el = '';
+  let r = document.querySelector('.element-details');
+  r.style.display = "flex";
 
+  // following code removes the previous image and paragraph text
+  if (r.hasChildNodes()) {
+    r.innerHTML = '';
+  }
 
-document
-  .querySelector('.element-details')
-  .addEventListener('click', (ev) => {
-  document.querySelector('.element-details').style.display = "none";
-})
+  data.forEach(element => {
+    if (Object.keys(element)[0] === m) {
+      el = element;
+    }
+  });
 
-document
-  .getElementById("he")
-  .addEventListener('click', () => {
-  document.querySelector('#helium-details').style.display = "flex";
-})
+  if (el) {
+    let i = new Image();
+    i.src = el[m][0];
+    r.append(i);
+    let p = document.createElement('p');
+    p.textContent = el[m][1];
+    r.append(p);
+  } else {
+    r.textContent = "Data not available !";
+  }
+}
 
-
-document
-  .querySelector('#helium-details')
-  .addEventListener('click', () => {
-  document.querySelector('#helium-details').style.display = "none";
-})
+let n = document.querySelectorAll('.n-element');
+n.forEach(e => e.addEventListener('click', () => {
+  let m = e.children[2].textContent.trim();
+  showDetails(m);
+}));
