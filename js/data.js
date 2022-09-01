66c43ef944152c4375301290a737a8e817ae6e76
barungh <barungh@hotmail.com>
2022-09-01 10:41:10 +0530

    New Js code and new data structure 🎈

--- js/data.js
+++ js/data.js
@@ -1,23 +1,58 @@
-const data = [ 
-  {
-  "Hydrogen" : [ "../images/nebula.jpg", 
-    `Hydrogen is the number one element of the periodic table and earns
-this status for a range of reasons: along with helium and lithium, it
-was one of the first three elements produced during the BigBang; it
-is the most abundant element in the universe, accounting for 88
-percent of all atoms; and it is the lightest of all the elements, with
-only one proton (which is why it is number 1 in the periodic table)
-and one electron`
-  ]
-},
-  {
-    "Helium" : [ "../images/jupiter.png",
-    `Helium is named for the Greek God of the sun, Helios, after it was
-first detected as unknown yellow spectral lines in sunlight. In India
-in 1868, French astronomer Jules Janssen passed sunlight through
-a prism during a solar eclipse (to split the light into its component
-colors). He found a sudden jump in the brightness of yellow light,
-which he initially believed to be caused by sodium`
-      ]
-  }
-] 
+// const data = [ 
+//   {
+//   "Hydrogen" : [ "../images/nebula.jpg", 
+//     `Hydrogen is the number one element of the periodic table and earns
+// this status for a range of reasons: along with helium and lithium, it
+// was one of the first three elements produced during the BigBang; it
+// is the most abundant element in the universe, accounting for 88
+// percent of all atoms; and it is the lightest of all the elements, with
+// only one proton (which is why it is number 1 in the periodic table)
+// and one electron`
+//   ]
+// },
+//   {
+//     "Helium" : [ "../images/jupiter.png",
+//     `Helium is named for the Greek God of the sun, Helios, after it was
+// first detected as unknown yellow spectral lines in sunlight. In India
+// in 1868, French astronomer Jules Janssen passed sunlight through
+// a prism during a solar eclipse (to split the light into its component
+// colors). He found a sudden jump in the brightness of yellow light,
+// which he initially believed to be caused by sodium`
+//       ]
+//   }
+// ] 
+
+
+const data = {
+  "Hydrogen" : ["https://hydrogen-central.com/wp-content/uploads/2021/05/hydrogen-natural-gas-740x493.png", 
+    `Named after the Greek words hydro for "water" and genes for "forming,"
+        Melting point: minus 434.7 degrees Fahrenheit (minus 259.34 degrees Celsius)
+        Boiling point: minus 423.2 F (minus 252.87 C)
+        Atomic weight (average mass of the atom): 1.00794
+        Phase at room temperature: Gas
+        Robert Boyle produced hydrogen gas in 1671 while he was experimenting with iron and acids
+        The element was named hydrogen by the French chemist Antoine Lavoisier.
+        It is found in the sun and most of the stars, and the planet Jupiter is composed mostly of hydrogen
+        On Earth, hydrogen is found in the greatest quantities as water. 
+        It is present as a gas in the atmosphere only in tiny amounts – less than 1 part per million by volume.`
+  ],
+  "Helium": [
+    "https://cdn.mos.cms.futurecdn.net/H3wgQVehsRLqAJmuYaN94M-970-80.jpg.webp",
+    `
+        French astronomer Pierre Janssen was on-hand in India to watch, and to measure the sun's atmosphere, known as the chromosphere.
+        Melting point: minus 458.0 degrees Fahrenheit (minus 272.2 degrees Celsius)
+        Boiling point: minus 452.07 F (minus 268.93 C)
+        Atomic weight (average mass of the atom): 4.002602
+        Phase at Room temperature: Gas
+        On the Earth, most helium is a radioactive decay product of uranium and thorium. It is found under the Earth's crust with other natural gases. 
+        Helium is used for medicine, scientific research, arc welding, refrigeration, gas for aircraft, coolant for nuclear reactors, cryogenic research
+        and detecting gas leaks.
+        NASA uses helium as an inert purge gas for hydrogen systems and a pressurizing agent for ground and flight fluid systems. 
+        Helium has many unique properties: low boiling point, low density, low solubility, high thermal conductivity and inertness
+        Helium was the first gas used for filling balloons and dirigibles.
+    `
+  ],
+  "Lithium" : ['https://ccg1lithium.weebly.com/uploads/1/9/5/8/19586997/header_images/1367668511.jpg', `
+    Canada had limited production of lithium from 2014 to 2019 and none in 2020. Several companies are currently working to develop lithium projects from traditional hard rock mining to unconventional sources such as oilfield brines and industrial wastewaters, in Ontario, Quebec, Alberta, Manitoba and Saskatchewan. These projects range from early exploration to an advanced stage.
+    `]
+}
--- js/index.js
+++ js/index.js
@@ -1,37 +1,34 @@
-let showDetails = (iSrc='', pText='') => {
-  let r = document.querySelector('.element-details');
-  r.style.display = "flex";
-  r.style.color = "initial";
-  // following code removes the previous image and paragraph text
-  if (r.hasChildNodes()) {
-    r.innerHTML = '';
+// hare krishna
+let showResult = (details) => {
+  let myDiv = document.querySelector(".element-details");
+  myDiv.style.display = "flex";
+  myDiv.style.color = "initial";
+
+  if (myDiv.hasChildNodes()) {
+    myDiv.innerHTML = '';
   }
 
-  if (iSrc && pText) {
-    let i = new Image();
-    i.src = iSrc;
-    r.append(i);
-    let p = document.createElement('p');
-    p.textContent = pText;
-    r.append(p);
+
+  if (details) {
+  let i = new Image;
+  i.src = details[0];
+  myDiv.append(i);
+
+  let p = document.createElement('p');
+  p.textContent = details[1];
+  myDiv.append(p);
   } else {
-      r.style.color = "#ff4000";
-      r.textContent = "Data Not Available !";
+    myDiv.style.color = "#ff4000";
+    myDiv.textContent = "Data not available !";
   }
+
 }
 
+
 document.body.addEventListener('click', (ev) => {
-  if (ev.target.className === "box-content" || ev.target.className === "box-footer") {
-    let e = ev.target.parentElement;
-    let i = parseInt(e.children[0].children[0].innerText);
-    i = i-1;
-    let el = data[i];
-    if (el) {
-      let iSrc = Object.values(el)[0][0];
-      let pText = Object.values(el)[0][1];
-      showDetails(iSrc, pText);
-    } else {
-      showDetails('','');
-    }
+  if (ev.target.className === "box-content") {
+    let el = ev.target.nextElementSibling.textContent.trim();
+    let d = data[el];
+    showResult(d);
   }
 })
